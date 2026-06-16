89b152d710e1f530f3af0a42801fe974b95ee5df
emaldev <261611436+emaldev@users.noreply.github.com>
2026-06-17 04:08:47 +0430

    Add PrimeChecker exercise

---
+++ PrimeChecker.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class PrimeChecker {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+    
+        System.out.println("Enter a number : ");
+        int number = input.nextInt();
+  
+        boolean isPrime = true;
+        if(number <=1 ){
+            isPrime = false;
+        }else {
+            for(int i = 2 ; i < number; i++){
+                if(number % i == 0){
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+        if(isPrime){
+            System.out.println(number + " is a Pime number .");
+        }else {
+            System.out.println(number + " is not a prime number.");
+        }
+        input.close();
+    }
+}
